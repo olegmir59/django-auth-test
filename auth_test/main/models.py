1f9b5e56ce40fecd24a94a292426b316d04fdc9a
olegmir59 <udom1@ya.ru>
2025-09-07 22:06:19 +0700

    replace User  --> CustomUser

--- auth_test/main/models.py
+++ auth_test/main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+# Кастомная модель
 class CustomUser(AbstractUser):
     phone_number = models.CharField("Номер телефона", max_length=15, unique=True)
     email = models.EmailField("Электронная почта", unique=True)
