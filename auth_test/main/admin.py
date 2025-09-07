581d5e8b1715eddbc3776186b39eac2f2e5a015d
olegmir59 <udom1@ya.ru>
2025-09-07 18:29:45 +0700

    modify admin.py

--- auth_test/main/admin.py
+++ auth_test/main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User  # замените 'ваше_приложение' на реальное имя
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'phone_number', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('username', 'email', 'phone_number')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone_number')}),
+        (_('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions',
+            ),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone_number', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
