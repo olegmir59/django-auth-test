9b855a0fc1e878cb3ef0272e9d050485449051b7
olegmir59 <udom1@ya.ru>
2025-09-07 20:47:42 +0700

    modify admin.py

--- auth_test/main/admin.py
+++ auth_test/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
-from .models import User  # замените 'ваше_приложение' на реальное имя
+from .models import User
 
 
 class UserAdmin(BaseUserAdmin):
